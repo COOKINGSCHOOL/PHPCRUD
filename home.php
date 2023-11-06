fa63ee36e80d3909f927b68c99d3edd83c7dedd0
Anthony Carlos Amutária de Carvalho <anthony343522@gmail.com>
2023-11-06 17:58:26 -0300

    novo

--- home.php
+++ home.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SITE</title>
     <style>
+        
         body {
             font-family: Arial, Helvetica, sans-serif;
             background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
